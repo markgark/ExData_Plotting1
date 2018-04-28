b819771eadb06767806980c8701481347896ad10
markgark <mvgarcia99@hotmail.com>
2018-04-28 00:57:55 -0500

    Update Data Loading Section

--- plot4.R
+++ plot4.R
@@ -1,3 +1,21 @@
+# Load data from file: household_power_consumption.txt
+hpc <- read.table("household_power_consumption.txt",header=T,sep=";",na.strings="?")
+
+#  Set format date
+hpc$Date <- as.Date(hpc$Date,"%d/%m/%Y")
+
+# Dates ranage
+dates <- c("2007-02-01","2007-02-02")
+
+# Date format conversion
+dates <- as.Date(dates)
+
+# Subsetting the data
+hhpc <- subset(hpc,Date %in% dates)
+
+# Code to adapt date and time
+hhpc <- within(hhpc, Datetime <- strptime(paste(as.character(hhpc$Date), hhpc$Time), format="%Y-%m-%d %H:%M:%S"));
+
 # Plot4
 
 png(filename = "plot4.png",width = 480, height = 480, units = "px")
